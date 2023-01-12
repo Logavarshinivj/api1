62a7320b016899114b72ce186bbdfe9161333329
Logavarshinivj <118879051+Logavarshinivj@users.noreply.github.com>
2023-01-12 21:42:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var url="https://animechan.vercel.app/api/random"
+fetch(url)
+.then((res) => res.json())
+.then((quote)=>console.log(quote))
+var url="https://animechan.vercel.app/api/random"
+async function fetchingapi(){
+var fetch_url=await fetch(url)
+var res=await fetch_url.json()
+console.log(res)
+
+
+var child_ele=document.createElement("p")
+child_ele.innerHTML=res.anime
+document.querySelector('body').appendChild(child_ele)
+
+var child=document.createElement("div")
+child.innerHTML+=`
+<p>anime:${res.anime}</P>
+<p>character:${res.character}</P>
+<p>quote:${res.quote}</P>
+`
+document.querySelector('body').appendChild(child)
+}
+fetchingapi()
+
+
+
+
+
